27e3d5334006b97cc7cf24cf736f142663aa00d1
Cameron McCormack <cameron.mccormack@softwire.com>
2022-06-18 09:54:23 +0100

    compute fibonnaci numbers in an array

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -36,3 +36,8 @@ const computeNegativeFibonacci = (position: number): number => {
     const absoluteResult = computeFibonacciNumber(-position);
     return resultIsNegative ? absoluteResult * -1 : absoluteResult;
 }
+
+export const computeFibonacciArray = (start: number, endInclusive: number): number[] => {
+    const inputArray = [...Array(endInclusive - start + 1).keys()].map(i => i + start);
+    return inputArray.map(x => computeFibonacciNumber(x));
+}
